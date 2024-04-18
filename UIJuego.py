1543cc9dd0a5185712736e2a788dd0045c739be4
santiagoMosquera99 <krakedev3@gmail.com>
2024-04-17 21:14:37 -0500

    Cortas mejoras

--- UIJuego.py
+++ UIJuego.py
@@ -1,5 +1,8 @@
 from tkinter import *
 from juego import *
+
+elementoObtenido = []
+
 ventana = Tk()
 ventana.title("Juego")
 ventana.iconbitmap("atomo.ico")
@@ -8,16 +11,27 @@ ventana.resizable(0,0)
 ventana.config(bg="#222")
 # Funciones
 def mostrarCompuesto():
+  global elementoObtenido
   infoElemento = escojerElemento()
+  elementoObtenido = infoElemento
   elemento = infoElemento[0]
   print(elemento)
   lblElemento.config(text=elemento)
+def compararElemento():
+  elementoUsuario = txtCompuesto.get()
+  global elementoObtenido
+  print(elementoObtenido,elementoUsuario)
+  respuesta = validarElementos(elementoObtenido,elementoUsuario)
+  print(respuesta)
 # FRAMES
 frameJuego = Frame(ventana, bg="#323232")
 lbltitulo = Label(text="Memorize",bg="#222",fg="#fff",foreground="white",font=("Arial",32),pady=10,).pack()
 lblElemento = Label(text="",bg="#222",fg="#fff",font=("Arial",24),pady=24,padx=24,borderwidth=(4),relief="solid",highlightcolor="white",highlightthickness=1)
 lblElemento.pack()
-lblCompuesto = Label(text="Hidrogeno",bg="#222",fg="#fff",font=("Arial",24),pady=32,).pack()
-txtCompuesto = Entry(bg="#333",fg="White",font="Arial",highlightthickness=1,highlightcolor="White").pack()
-btnConfirmar = Button(text="Comprobar",command=mostrarCompuesto).pack()
+lblCompuesto = Label(text="?",bg="#222",fg="#fff",font=("Arial",24),pady=32,).pack()
+txtCompuesto = Entry(bg="#333",fg="White",font="Arial",highlightthickness=1,highlightcolor="White")
+txtCompuesto.pack()
+lblError = Label(text="",pady=24,bg="#222").pack()
+btnConfirmar = Button(text="Comprobar",command=compararElemento,pady=4,bg="#0af",fg="white").pack()
+mostrarCompuesto()
 ventana.mainloop()
--- juego.py
+++ juego.py
@@ -2,11 +2,13 @@ from random import *
 
 elementos= ["H-Hidrogeno-1-1-1-50",
              "He-Helio-2-18-1-50",
-             "Ne-Neòn-10-18-2-50",
-             "Ar-Argòn-18-18-3-50",
-             "Kr-Kriptòn-36-18-4-50",
-             "Xe-Xenòn-54-18-5-50",
-             "Rn-Radòn-86-18-6-50"]
+             "Ne-Neon-10-18-2-50",
+             "Ar-Argon-18-18-3-50",
+             "Kr-Kripton-36-18-4-50",
+             "Xe-Xenon-54-18-5-50",
+             "Rn-Radon-86-18-6-50"]
+def todosElementos():
+    return elementos
 def escojerElemento ():
     i = 0
     while i<1:
@@ -21,21 +23,26 @@ def escojerElemento ():
         else:
             elementos.append(elemento)
             
-infElemento=escojerElemento()
-""" print(infElemento [0])
-respuesta =input("cual es el nombre del elemento:")
-probalElemento=int(infElemento.pop())
-if infElemento[1] == respuesta:
-    if probalElemento >10:
-        probalElemento = probalElemento-10
-        infElemento.append(probalElemento)
+def validarElementos(infElemento,respuesta):
+    print(infElemento)
+    probalElemento=int(infElemento.pop())
+    print(f"Prob {probalElemento}")
+    Elemento = str(infElemento[1])
+    elementoLower = Elemento.lower()
+    respuestaStr = str(respuesta)
+    respuestaFinal = respuestaStr.lower()
+    if elementoLower == respuestaFinal:
+        if probalElemento >10:
+            probalElemento = probalElemento-10
+            infElemento.append(probalElemento)
+        else:
+            infElemento.append(probalElemento)
     else:
-        infElemento.append(probalElemento)
-else:
-    if probalElemento < 100:
-        probalElemento = probalElemento+10
-        infElemento.append(probalElemento)
-    else:
-        infElemento.append(probalElemento)
-elemento= infElemento[0]+"-"+infElemento[1]+"-"+infElemento[2]+"-"+infElemento[3]+"-"+infElemento[4]+"-"+str(infElemento[5])
-elementos.append(elemento) """
+        if probalElemento < 100:
+            probalElemento = probalElemento+10
+            infElemento.append(probalElemento)
+        else:
+            infElemento.append(probalElemento)
+    elemento = infElemento[0]+"-"+infElemento[1]+"-"+infElemento[2]+"-"+infElemento[3]+"-"+infElemento[4]+"-"+str(infElemento[5])
+    elementos.append(elemento)
+    return elemento
